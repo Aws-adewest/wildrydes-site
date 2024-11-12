1af240c4fed867fd6ff6244891897cbb3ebe7821
Sulaimon Olayemi <167693375+Aws-adewest@users.noreply.github.com>
2024-11-12 14:15:20 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: "us-east-1_2dOaC4Hyb", // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: "508enkf8e4qms49n73s46s2j4k", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: ''us-east-1" // e.g. us-east-2
+          userPoolId: 'us-east-1_827Z9qGN6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2epdnqi6s5m69ns1gqp4fe8591', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1'// e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
